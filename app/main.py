341100ec0ccf963e77c7269a85818c582ea063b4
Mahir Asef <darkknight5812@gmail.com>
2026-06-20 01:22:05 +0600

    wait for menu func

--- app/main.py
+++ app/main.py
@@ -3,12 +3,15 @@ from app.services import task_service
 from rich.console import Console
 console = Console()
 
+def wait_for_menu():
+    console.input("[bold yellow]Press Enter to return to the menu...[/bold yellow]")
+
 def main():
     init_db.initialize_database()
 
     while True:
         console.print("[bold green]Welcome to your personal Task Manager [/bold green]")
-        console.print("[bold magenta]-------------- DevTack -------------- [/bold magenta]")
+        console.print("[bold green]-------------- DevTack -------------- [/bold green]")
         print("\n")
 
         console.print("[bold yellow]1. Add a Task [/bold yellow]")
@@ -30,37 +33,43 @@ def main():
                 console.print(f"[bold cyan]New task added with task id : {new_task_id} [/bold cyan]")
             else:
                 console.print("[bold red] Invalid title [/bold red]")
+            wait_for_menu()
     
         elif user_inp == 2:
             console.print("[bold green]Displaying the list of the tasks: \n [/bold green]")
             task_list = task_service.list_tasks()
             for task in task_list:
                 task_id, title, status, created_at, completed_at = task 
-                console.print(f"[bold cyan]{task_id}. {title} - {status} [/bold cyan]")
+                console.print(f"[bold cyan]{task_id}. {title} - {status} [/bold cyan]\n")
+            wait_for_menu()
     
         elif user_inp == 3:
             try: 
                 task_id = int(console.input("[bold green]Provide the task id of the task you want to mark as completed: [/bold green]"))
             except ValueError as e:
                 console.print("[bold red]Invalid task id [/bold red]")
+                wait_for_menu()
                 continue 
             complete_taskId = task_service.complete_task(task_id)
             if complete_taskId:
                 console.print("[bold cyan]Task Completed [/bold cyan]")
             else: 
                 console.print("[bold red]Invalid task id [/bold red]")
+            wait_for_menu()
     
         elif user_inp == 4:
             try: 
                 task_id = int(console.input("[bold green]Provide the task id of the task you want to delete: [/bold green]"))
             except ValueError as e:
                 console.print("[bold red]Invalid task id [/bold red]")
+                wait_for_menu()
                 continue 
             delete_taskId = task_service.delete_task(task_id)
             if delete_taskId:
                 console.print("[bold cyan]Task Deleted [/bold cyan]")
             else: 
                 console.print("[bold red]Invalid task id [/bold red]")
+            wait_for_menu()
 
         elif user_inp == 5:
             console.print("[bold green]Goodbye [/bold green]")
@@ -68,6 +77,7 @@ def main():
 
         else:
             console.print("[bold red]Enter a valid digit between 1 to 5 [/bold red]")
+            wait_for_menu()
 
 
 if __name__ == "__main__":
